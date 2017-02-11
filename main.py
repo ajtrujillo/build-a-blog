55cc3e521c1a3ff2dd0d33424e9a73e720230327
A.J. Trujillo <ajtrjll@gmail.com>
2017-02-11 13:05:20 -0600

    Jinja initialization, basic Handler class.

--- main.py
+++ main.py
@@ -15,11 +15,30 @@
 # limitations under the License.
 #
 import webapp2
+import cgi
+import jinja2
+import os
+from google.appengine.ext import db
 
-class MainHandler(webapp2.RequestHandler):
+# set up jinja
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *args, **kwargs):
+        self.response.out.write(*args, **kwargs)
+
+    def render_str(selfself, template, **params):
+        t=jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kwargs):
+        self.write(self.render_str(template, **kwargs))
+
+class MainPage(Handler):
     def get(self):
-        self.response.write('Hello world!')
+        self.render('Hello world!')
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', Handler), ('/blog', BlogHandler), ('/newpost', NewPostHandler)
 ], debug=True)
