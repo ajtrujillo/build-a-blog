3692df7af57e152d9ad2709bcf34321ba009c019
A.J. Trujillo <ajtrjll@gmail.com>
2017-02-12 14:48:00 -0600

    Last round of edits before I pull it apart into separate Handler classes.

--- main.py
+++ main.py
@@ -1,19 +1,3 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
 import jinja2
 import os
@@ -55,14 +39,19 @@ class MainPage(Handler):
         title = self.request.get("title")
         wordart = self.request.get("wordart")
 
-        if title and wordart:
+        if title == True and wordart == True:
             w = UserSubmission(title=title, wordart=wordart)
             w.put()
-
             self.redirect("/")
         else:
             error = "We require your title and your blog content."
             self.render_blog(title, wordart, error)
+
+class NewPostHandler(MainPage):
+
+
+
+
 
 # class ViewPostHandler(webapp2.RequestHandler):
 #     def get(self, id):
@@ -71,6 +60,6 @@ class MainPage(Handler):
 app = webapp2.WSGIApplication([
     ('/', MainPage),
     #('/blog', BlogHandler),
-    #('/newpost', NewPostHandler),
+    ('/newpost', NewPostHandler),
     #(webapp2.Route('/blog/<id:\d+>', ViewPostHandler))
 ], debug=True)
