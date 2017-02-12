8d40438861bb3f5cd4e6c27aa1c450b2d327e533
A.J. Trujillo <ajtrjll@gmail.com>
2017-02-12 15:04:02 -0600

    lost some changes, dont get it, now fixing and committing again.

--- main.py
+++ main.py
@@ -1,19 +1,3 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
 import jinja2
 import os
@@ -58,8 +42,7 @@ class MainPage(Handler):
         if title and wordart:
             w = UserSubmission(title=title, wordart=wordart)
             w.put()
-
-            self.redirect("/")
+            self.redirect("newpost.html")
         else:
             error = "We require your title and your blog content."
             self.render_blog(title, wordart, error)
